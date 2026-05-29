468425e5db6ec3fc2ff6c8aa9089b0d09c24b870
Kitsolitelu <ST10486804@rcconnect.edu.za>
2026-05-29 14:20:42 +0200

    the wpf entry point that lauches the MainWindow

---
+++ App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PROG_P2_CHATBOT
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    /// this wpf  entry point that aunches the MainWindow 
+     
+    public partial class App : Application
+    {
+    }
+}
